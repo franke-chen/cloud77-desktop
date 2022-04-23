08c76ebf92207fe222dc8af9709d12cb3e730908
franke-chen <85813879+franke-chen@users.noreply.github.com>
2022-04-24 00:46:21 +0800

    fix (#6)

--- download.ps1
+++ download.ps1
@@ -2,13 +2,11 @@
 
 $url = 'https://www.cloud77.top/statics/squirrel.zip'
 Invoke-RestMethod -Uri $url -OutFile ./squirrel.zip
-
-$url = 'https://www.cloud77.top/statics/cloud77-wpf/stable/releases.zip'
-Invoke-RestMethod -Uri $url -OutFile ./releases.zip
-
 mkdir squirrel
 Expand-Archive -LiteralPath .\squirrel.zip -DestinationPath .\squirrel
 
+$url = 'https://www.cloud77.top/statics/cloud77-wpf/stable/releases.zip'
+Invoke-RestMethod -Uri $url -OutFile ./releases.zip
 mkdir releases
 Expand-Archive -LiteralPath .\releases.zip -DestinationPath .\releases
 
